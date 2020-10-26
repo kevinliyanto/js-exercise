0a9b5b614f4246cffe69f3963331aadf2cb729d7
kevinliyanto <kevinliyanto@hotmail.com>
2020-10-26 23:37:08 +1100

    Minify the test function and removed swap.js content

--- exercise/04/round.js
+++ exercise/04/round.js
@@ -2,19 +2,15 @@ main();
 
 // Do not change this function
 function main() {
-  const e = [0.95, -2.2, 8, 1.3, -5.5, -14, 7.5, 6.8, 43, -2.35];
-  const f = [...e];
-  const [g, h] = [round, Math.round];
+  const e = [0.95, -2.2, 8, 1.3, -5.5, -14, 7.5, 6.8, 43, -2.35],
+    f = [...e],
+    [g, h] = [round, Math.round];
   g(e);
-  let [s, t] = [true, true];
-  for (let i = 0; i < e.length; i++) {
-    e[i] % 1 !== 0 && (s = false);
-  }
-  for (let i = 0; i < f.length; i++) {
-    e.indexOf(h(f[i])) !== -1 || (t = false);
-  }
-  console.log(`Elements in your array are ${s ? "" : "not "}rounded`);
-  if (!t) console.log(`You have modified a value on your array`);
+  let [s, t] = [!0, !0];
+  for (let o = 0; o < e.length; o++) e[o] % 1 != 0 && (s = !1);
+  for (let o = 0; o < f.length; o++) -1 !== e.indexOf(h(f[o])) || (t = !1);
+  console.log(`Elements in your array are ${s ? "" : "not "}rounded`),
+    t || console.log("You have modified a value on your array");
 }
 
 // Modify the functions below
--- exercise/04/swap.js
+++ exercise/04/swap.js
@@ -2,29 +2,18 @@ main();
 
 // Do not change this function
 function main() {
-  const e = [0.95, -2.2, 8, 1.3, -5.5, -14, 7.5, 6.8, 43, -2.35];
-  const f = [...e];
-  const v = swapFirstSecond;
+  const e = [0.95, -2.2, 8, 1.3, -5.5, -14, 7.5, 6.8, 43, -2.35],
+    f = [...e],
+    v = swapFirstSecond;
   v(e);
-  let [s, t] = [true, true];
-  for (let i = 0; i < e.length; i += 2) {
-    e[i] > e[i + 1] && (s = false);
-  }
-  for (let i = 0; i < f.length; i++) {
-    f.indexOf(e[i]) !== -1 || (t = false);
-  }
-  console.log(`Your array is ${s ? "" : "not "}correct`);
-  if (!t) console.log(`You have modified a value on your array`);
+  let [s, t] = [!0, !0];
+  for (let o = 0; o < e.length; o += 2) e[o] > e[o + 1] && (s = !1);
+  for (let o = 0; o < f.length; o++) -1 !== f.indexOf(e[o]) || (t = !1);
+  console.log(`Your array is ${s ? "" : "not "}correct`),
+    t || console.log("You have modified a value on your array");
 }
 
 // Modify the functions below
-function swapFirstSecond(k) {
+function swapFirstSecond(array) {
   // Put your code here
-  for (let i = 0; i < k.length; i += 2) {
-    if (k[i] > k[i + 1]) {
-      const f = k[i];
-      k[i] = k[i + 1];
-      k[i + 1] = f;
-    }
-  }
 }
